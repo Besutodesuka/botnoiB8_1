4dcff8227b49b00b4a98ef1d74399909ba0856b9
Besutodesu <60954224+Besutodesuka@users.noreply.github.com>
2021-03-09 22:33:48 +0700

    Add files via upload

---
+++ predict_image.py
@@ -0,0 +1,14 @@
+import botnoi as bn
+import pickle
+from botnoi import cv
+import numpy as np
+
+### load model
+modFile = 'mymod.mod'
+mod = pickle.load(open(modFile,'rb'))
+
+def predicting(imgurl):
+  a = cv.image(imgurl)
+  feat = a.getresnet()
+  res = mod.predict([feat])[0]
+  return res
